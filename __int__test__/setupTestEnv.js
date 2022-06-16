50f44ca4bc560c0ffeafea2ccc26972873ab2592
AdmirDemaj <adidemaj@yahoo.com>
2022-06-16 20:17:23 +0200

    Day4 Changes

---
+++ __int__test__/setupTestEnv.js
@@ -0,0 +1,36 @@
+
+const {build} = require("../src/app")
+
+const createTableSQL="CREATE TABLE IF NOT EXISTS todos ( id SERIAL, name VARCHAR(200),description VARCHAR(500), gross_amount NUMERIC, net_amount NUMERIC, exluded_vat_amount NUMERIC, PRIMARY KEY(id));";
+ 
+
+const clearTableSQL = "DELETE FROM todos";
+
+const insertFakeItemSQL = "INSERT INTO todos (name, description, gross_amount, net_amount, exluded_vat_amount) VALUES($1, $2, $3, $4, $5)";
+
+module.exports = function setUpTestEnv(){
+    const app = build({logger: true}, 
+        {},
+        {connectionString: 'postgres://postgres:postgres@localhost:5432/postgres_test'})
+
+        beforeAll(async ()=> {
+            await app.ready()
+            await app.pg.query(createTableSQL)
+            await app.pg.query(clearTableSQL)
+
+        })
+
+        beforeEach(async () => {
+            await app.pg.query(insertFakeItemSQL,["Test Todo", "This is a test item", 20, 16.67, 3.33])
+        })
+
+        afterEach(async () =>{
+            await app.pg.query(clearTableSQL)
+        })
+
+        afterAll(async () =>{
+            app.close()
+        })
+        return app
+
+    }
--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const fastify = require ('fastify')
 const fastifySwagger = require('@fastify/swagger')
+const fastifyPostgres = require('@fastify/postgres')
 
 const {ToDoRoute} = require('./routes/todos')
+//const {itemRoute} = require('./routes/items')
+const {todoRoute_v2} = require('./routes/v2/todos')
 
 
-const build = (opts={}, optsSwagger={}) => {
+const build = (opts={}, optsSwagger={}, optsPostgres={}) => {
     const app = fastify(opts)
+    app.register(fastifyPostgres,optsPostgres)
     app.register(fastifySwagger,optsSwagger)
-    app.register(ToDoRoute)
+    app.register(ToDoRoute, {prefix: '/v1'})
+   // app.register(ToDoRoute, {prefix: '/v1'})
+   app.register(todoRoute_v2, {prefix: '/v2'})
     return app
 }
 
